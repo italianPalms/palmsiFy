42ba19148bd1140b082ab65ee9409f50d9913c75
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-09-09 14:36:10 +0200

    add labels and input fields for the signup page, and button and link to login page

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,8 +1,40 @@
 
 export default function Signup() {
     return (
-        <div className="">
-        <h1 className="text-3xl">Signup</h1>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1 className="text-4xl font-semibold mb-3">Signup</h1>
+
+        <label className="text-xl font-medium mt-3">Username</label>
+        <input
+        className="p-2 mt-2 text-black"
+        id="username"
+        type="username"
+        placeholder="Enter your username"></input>
+
+        <label className="text-xl font-medium mt-3">Email</label>
+        <input
+        className="p-2 mt-2 text-black"
+        id="email"
+        type="email"
+        placeholder="Enter your email">
+        </input>
+
+        <label className="text-xl font-medium mt-3">Password</label>
+        <input
+        className="p-2 mt-2 mb-2 text-black"
+        id="password"
+        type="password"
+        placeholder="Enter your password"></input>
+
+        <button className="border-2 mt-10 p-2 w-48 bg-sky-400"
+        onClick={() => {
+            console.log("Signup button clicked")
+        }}
+        >Signup</button>
+
+        <a href="/login" className="p-2 font-medium text-base">Already a user? Login here!</a>
+
         </div>
+
     )
 }
