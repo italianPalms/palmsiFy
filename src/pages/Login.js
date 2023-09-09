8494468f189af8b01a42c486b70060667324886d
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-09-09 14:50:40 +0200

    add login fields and styling

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,30 @@
 export default function Login() {
     return (
-        <>
-        <h1>Login</h1>
-        </>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1 className="text-4xl font-semibold mb-3">Login</h1>
+
+        <label className="text-xl font-medium mt-3">Email</label>
+        <input
+        className="p-2 mt-2 text-black rounded"
+        id="email"
+        type="email"
+        placeholder="Enter your email"></input>
+
+        <label className="text-xl font-medium mt-3">Password</label>
+        <input
+        className="p-2 mt-2 text-black rounded"
+        id="password"
+        type="password"
+        placeholder="Enter your password"></input>
+
+        <button className="border-2 mt-8 p-2 w-48 bg-sky-400"
+        onClick={() => {
+            console.log("Login button clicked")
+        }}
+        >Login</button>
+
+        <a href="/signup" className="p-2 font-medium text-base">Not a user? Signup here!</a>
+
+        </div>
     )
 }
