4566ba7d50e10706903ca238c8c7bc58cd9a47cb
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-09-09 14:50:59 +0200

    adjust margin for button

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -6,14 +6,14 @@ export default function Signup() {
 
         <label className="text-xl font-medium mt-3">Username</label>
         <input
-        className="p-2 mt-2 text-black"
+        className="p-2 mt-2 text-black rounded"
         id="username"
         type="username"
         placeholder="Enter your username"></input>
 
         <label className="text-xl font-medium mt-3">Email</label>
         <input
-        className="p-2 mt-2 text-black"
+        className="p-2 mt-2 text-black rounded"
         id="email"
         type="email"
         placeholder="Enter your email">
@@ -21,12 +21,12 @@ export default function Signup() {
 
         <label className="text-xl font-medium mt-3">Password</label>
         <input
-        className="p-2 mt-2 mb-2 text-black"
+        className="p-2 mt-2 text-black rounded"
         id="password"
         type="password"
         placeholder="Enter your password"></input>
 
-        <button className="border-2 mt-10 p-2 w-48 bg-sky-400"
+        <button className="border-2 mt-8 p-2 w-48 bg-sky-400"
         onClick={() => {
             console.log("Signup button clicked")
         }}
