235526035e752944c07dbf8d2327659f4431cc88
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-09-09 13:36:03 +0200

    fix import statement

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
-import Signup from "./pages/Signup.js";
+import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import NoPage from "./pages/NoPage";
 
