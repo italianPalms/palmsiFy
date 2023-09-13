5c5bae130c94d87b6d3ac9588977b0c706737d1a
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-09-13 22:31:18 +0200

    add forget password button on top right

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,11 +14,16 @@ export default function Home() {
         navigate("/login")
     };
 
+    const forgotPassword = () => {
+        console.log("Forgot password clicked")
+        navigate("/ForgotPassword")
+    };
+
     return (
         <>
         <div
         style={{
-            backgroundImage: `url(${backgroundImage})`, 
+            // backgroundImage: `url(${backgroundImage})`, 
             backgroundSize: 'cover', 
             backgroundRepeat: 'no-repeat', 
             backgroundPosition: 'center center', 
@@ -29,21 +34,25 @@ export default function Home() {
         }}
         >
         <div className="flex items-center">
-        <h1 className=" text-5xl font-bold mr-10 text-zinc-50">Home page</h1>
+        <h1 className=" text-5xl font-bold mr-4 text-zinc-50">Home page</h1>
         
-        <button className="p-2 border-2 mr-3 w-28 bg-sky-400"
+        <button className="p-2 border-2 mr-3 w-48 bg-sky-400 hover:bg-sky-500"
             onClick={signup}>Signup</button>
 
-        <button className="p-2 border-2 ml-3 w-28 bg-sky-400"
+        <button className="p-2 border-2 ml-3 w-48 bg-sky-400 hover:bg-sky-500"
             onClick={login}>Login</button>
         </div>
         </div>
 
-
-
-
-        {/* add background picture later */}
-
+        <div 
+        style={{
+            position: 'absolute', 
+            top: '20px', 
+            right: '20px', 
+        }}>
+        <button className="p-2 border-2 w-48 bg-orange-700 hover:bg-orange-900"
+        onClick={forgotPassword}>Forgot Password</button>
+        </div>
         </>
     )
 }
