31908fc1ed6b7bec9b76a4791b9b89a5ab2ab577
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-10-15 20:59:33 +0200

    add some of the logic so the user can enter a number and set the grid size

--- src/pages/EtchASketch.jsx
+++ src/pages/EtchASketch.jsx
@@ -2,27 +2,41 @@ import { LoggedInHeader } from "../components/LoggedInHeader";
 import React, { useEffect, useState } from 'react';
 
 
-const EtchASketch = () => {
-    const onClick = () => {
-        console.log("Clicked");
-    }
-
-    const [gridSize, setGridSize] = useState({width: 0, height: 0});
-    const [cellSize, setCellSize] = useState({width: 0, height: 0});
+function EtchASketch() {
+    const [gridSize, setGridSize] = useState({rows: 20, cols: 20});
+    console.log("gridSize", gridSize);
+    const [grid, setGrid] = useState([]);
+    const [inputValue, setInputValue] = useState('');
 
     useEffect(() => {
-        const container = document.getElementById("container");
-        if(container) {
-            const { clientWidth, clientHeight } = container;
-            const cellWidth = clientWidth / 15;
-            const cellHeight = clientHeight / 15;
+        const availableWidth = window.innerWidth;
+        const availableHeight = window.innerHeight - 96;
+        const cellWidth = availableWidth / gridSize.cols;
+        const cellHeight = availableHeight / gridSize.rows;
 
-            setGridSize({ width: clientWidth, height: clientHeight });
-            setCellSize({ width: cellWidth, height: cellHeight });
+        const newGrid = Array.from({ length: gridSize.rows }, () => Array.from({ length: gridSize.cols }, () => ({
+            width: cellWidth, 
+            height: cellHeight, 
+        }))
+        );
+
+        setGrid(newGrid);
+    }, [gridSize]);
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    const createGrid = () => {
+        console.log("create grid button clicked");
+        const newSize = parseInt(inputValue, 10);
+
+        if(!isNaN(newSize) && newSize >= 1 && newSize <= 100) {
+            setGridSize({row: newSize, cols: newSize });
+        } else {
+            alert('Please enter a valid number between 1 and 100');
         }
-    }, []);
-
-    const grid = new Array(15).fill().map(() => new Array(15).fill(0));
+    };
 
     return (
         <>
@@ -34,24 +48,36 @@ const EtchASketch = () => {
             <h1 className="text-3xl font-medium pb-4 mt-10">Etch-a-sketch</h1>
                 <div className="main">
                     <div className="btns pt-2 font-medium flex justify-center">
+                        <input className="text-black"
+                        type="number"
+                        placeholder="Enter grid size (maximum 100):"
+                        value={inputValue}
+                        onChange={handleInputChange}
+                        ></input>
                         <button className="grid-size p-2"
-                        onClick={onClick}
+                        onClick={createGrid}
                         >Create Grid</button>
 
                         <button className="clear-grid p-2"
-                        onClick={onClick}
+                        onClick={(() => {
+                            console.log("clear button clicked");
+                        })}
                         >Clear</button>
 
                         <button className="pink p-2"
-                        onClick={onClick}
+                        onClick={(() => {
+                            console.log("pink button clicked");
+                        })}
                         >Pink Color</button>
 
                         <button className="rainbow p-2"
-                        onClick={onClick}
+                        onClick={(() => {
+                            console.log("rainbow button clicked");
+                        })}
                         >Rainbow Color</button>
                     </div>
                     <div id="container" className="container flex justify-center items-center flex-col min-h-screen pb-96">
-                        <div className="grid">
+                        <div className="grid" style={{ width: gridSize.width, height: gridSize.height }}> 
                             {grid.map((row, rowIndex) => (
                                 <div key={rowIndex} className="row">
                                     {row.map((cell, colIndex) => (
