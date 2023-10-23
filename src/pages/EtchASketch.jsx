51a09e8242d7ef3c8cebbc12722e821805ede085
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-10-23 22:50:19 +0200

    add logic to set etch-a-sketch grid when user enter value

--- src/pages/EtchASketch.jsx
+++ src/pages/EtchASketch.jsx
@@ -5,34 +5,46 @@ import React, { useEffect, useState } from 'react';
 function EtchASketch() {
     const [gridSize, setGridSize] = useState({rows: 20, cols: 20});
     console.log("gridSize", gridSize);
-    const [grid, setGrid] = useState([]);
-    const [inputValue, setInputValue] = useState('');
+
+    const initialGrid = Array.from({ length: gridSize.rows }, () => Array.from({ length: gridSize.cols }, () => ({
+        width: 1, 
+        height: 1,
+    }))
+    );
+
+    const [grid, setGrid] = useState(initialGrid);
 
     useEffect(() => {
         const availableWidth = window.innerWidth;
         const availableHeight = window.innerHeight - 96;
+        console.log('Available Width:', availableWidth);
+        console.log('Available Height: ', availableHeight);
+
         const cellWidth = availableWidth / gridSize.cols;
         const cellHeight = availableHeight / gridSize.rows;
+        console.log('Cell Width:', cellWidth);
+        console.log('Cell Height:', cellHeight);
 
         const newGrid = Array.from({ length: gridSize.rows }, () => Array.from({ length: gridSize.cols }, () => ({
             width: cellWidth, 
             height: cellHeight, 
         }))
         );
+        console.log('New Grid:', newGrid);
 
         setGrid(newGrid);
     }, [gridSize]);
 
-    const handleInputChange = (e) => {
-        setInputValue(e.target.value);
-    };
 
     const createGrid = () => {
         console.log("create grid button clicked");
-        const newSize = parseInt(inputValue, 10);
-
+        const newSize = prompt('Enter grid size (maximum 100):');
         if(!isNaN(newSize) && newSize >= 1 && newSize <= 100) {
-            setGridSize({row: newSize, cols: newSize });
+            const cellWidth = window.innerWidth / newSize;
+            const cellHeight = (window.innerHeight - 96) / newSize;
+
+            setGridSize({rows: newSize, cols: newSize, cellWidth, cellHeight });
+
         } else {
             alert('Please enter a valid number between 1 and 100');
         }
@@ -48,12 +60,13 @@ function EtchASketch() {
             <h1 className="text-3xl font-medium pb-4 mt-10">Etch-a-sketch</h1>
                 <div className="main">
                     <div className="btns pt-2 font-medium flex justify-center">
-                        <input className="text-black"
+                        {/* <input className="text-black"
                         type="number"
                         placeholder="Enter grid size (maximum 100):"
                         value={inputValue}
                         onChange={handleInputChange}
-                        ></input>
+                        ></input> */}
+                        
                         <button className="grid-size p-2"
                         onClick={createGrid}
                         >Create Grid</button>
@@ -77,7 +90,7 @@ function EtchASketch() {
                         >Rainbow Color</button>
                     </div>
                     <div id="container" className="container flex justify-center items-center flex-col min-h-screen pb-96">
-                        <div className="grid" style={{ width: gridSize.width, height: gridSize.height }}> 
+                        <div className="grid" style={{ width: gridSize.cols * gridSize.cellWidth, height: gridSize.rows * gridSize.cellHeight }}> 
                             {grid.map((row, rowIndex) => (
                                 <div key={rowIndex} className="row">
                                     {row.map((cell, colIndex) => (
