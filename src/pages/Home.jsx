cd6f76661643c366d41e3de0aeedfcae3a8b1dfd
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-11-02 21:20:51 +0100

    add airbot img and align img and fields next to each other

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import Quote from "../img/quote.webp";
+import airbot from "../img/airobot.png";
 
 export default function Home() {
     const navigate = useNavigate();
@@ -21,17 +22,23 @@ export default function Home() {
             <Header />
         </div>
 
-        <div className="flex flex-col items-center justify-center h-screen pb-80">
-            <img src={Quote} alt="Quote: It's like i have ESPN or something." className="w-96" />
+        <div className="flex flex-row justify-center items-center">
+            <div className="flex flex-col items-center justify-center h-screen w-96">
+                <img src={airbot} alt="Airbot"/>
+            </div>
+            
+            <div className="flex flex-col items-center justify-center">
+            <div className="home-heading mb-2">
+            <h1 className="p-2 text-5xl font-bold italic">Home page</h1>
+            </div>
 
-            <div className="flex items-center">
-            <h1 className=" text-5xl font-bold mr-4 text-zinc-50">Home page</h1>
-
-            <button className="p-2 border-2 w-48 bg-sky-400 hover:bg-sky-500"
+            <div className="login-btn flex-col items-center justify-center min-w-min">
+            <button className="p-2 mr-2 border-2 w-48 bg-sky-400 hover:bg-sky-500"
                 onClick={login}>Login</button>
             
-            <button className="p-2 border-2 ml-3 mr-3 w-48 bg-sky-400 hover:bg-sky-500"
+            <button className="signup-btn p-2 ml-2 border-2 w-48 bg-sky-400 hover:bg-sky-500"
                 onClick={signup}>Signup</button>
+            </div>
             </div>
         </div>
         </>
