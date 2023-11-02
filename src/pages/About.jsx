54766d01b30970f9857b444ff2fed75a311f3c6b
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-11-02 20:51:09 +0100

    add dummy text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,6 +9,9 @@ export default function About() {
 
         <div className=" flex flex-col font-medium items-center justify-center">
         <h1 className="text-3xl font-medium m-6 mt-28">About PalmsiFy</h1>
+        <div className="p-2 ml-10 mr-10">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
         </div>
         </>
     )
