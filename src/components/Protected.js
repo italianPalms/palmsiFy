46f2a7ef560e2d72040096d005b770fcf7a41388
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-11-19 20:11:05 +0100

    remove empty lines

--- src/components/Protected.js
+++ src/components/Protected.js
@@ -4,9 +4,7 @@ import { useCookies } from "react-cookie";
 
 const Protected = ({ children }) => {
     const [cookies] = useCookies(["access_token"]);
-
     const isAuthenticated = !!cookies.access_token;
-
     if (!isAuthenticated) {
         return <Navigate to ="/" replace />;
     }
