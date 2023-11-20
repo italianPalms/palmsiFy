9c050d2728124597d1c34ac28d86014087e55b8f
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-11-20 12:15:52 +0100

    move search icon from header to the movie page and delete the movie-header as this is not needed

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
 import { LoggedInHeader } from "../components/LoggedInHeader";
-import { MovieHeader } from "../components/MovieHeader";
 import Footer from "../components/Footer";
 
 export default function Movies() {
@@ -70,13 +69,18 @@ export default function Movies() {
                 <div className="mt-4 bg-gradient-to-r from-neutral-950 via-purple-950 to-neutral-950">
                     <div className="flex flex-col items-center justify-center pt-8 pb-8 text-4xl font-semibold">
                         <h1>Movies</h1>
-                        
                     </div>
-                    
                 </div>
                 <div className="pl-10 mb-8">
-                    <MovieHeader />
-                    </div>
+                    <button className=" relative p-1 m-3 mt-4 top-5"
+                        onClick={() => {
+                        console.log("Search icon clicked");
+                        }}>
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 50 50" fill="white">
+                        <path d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"></path>
+                        </svg>
+                    </button>
+                </div>
                 <div className="flex flex-wrap pl-10 pr-10 mb-10">
                     {movies.map((movie) => (
                     <div key={movie.id} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 p-4">
