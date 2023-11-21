8eb8b7613039807395f4d5e3f80a23ae50df3def
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-11-21 22:31:26 +0100

    add label, input field and dummy list

--- src/pages/Groceries.jsx
+++ src/pages/Groceries.jsx
@@ -7,7 +7,48 @@ export default function groceries() {
             <LoggedInHeader />
         </div>
         <div className="flex flex-col justify-center items-center mt-10">
-            <h1 className="text-3xl">Groceries</h1>
+            <h1 className="text-4xl font-bold">Groceries</h1>
+        </div>
+
+        <div className="flex flex-col justify-center items-center">
+            <label className="text-xl font-medium mt-3">Add item</label>
+            <input className="p-1 text-black mt-2 w-48" 
+                type="text" placeholder="Add item "></input>
+            <button className="p-1 mt-4 bg-sky-400 w-48 hover:bg-sky-500 text-white rounded"
+            onClick={() => {
+                console.log("Add button clicked")
+            }}
+            >Add</button>
+        </div>
+
+        <div className="flex flex-col justify-center items-center mt-10">
+            <ul>
+                <li>
+                    <label>
+                        <input
+                            type="checkbox" className=""
+                            onChange={() => {
+                                console.log("Checkbox clicked")
+                            }}
+                            ></input>
+                            
+                    </label>
+                Bananas
+                </li>
+                <li>
+                    <label>
+                        <input
+                            type="checkbox"
+                            onChange={() => {
+                                console.log("Checkbox clicked")
+                            }}
+                            ></input>
+                            
+                    </label>
+                Milk
+                </li>
+                
+            </ul>
         </div>
         </>
     )
