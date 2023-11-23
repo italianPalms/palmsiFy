d22529b92f150de862d5abe7271cd47d3547cd1b
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-11-23 22:10:44 +0100

    add logic to add groceries to a list

--- src/pages/Groceries.jsx
+++ src/pages/Groceries.jsx
@@ -1,6 +1,31 @@
 import { LoggedInHeader } from "../components/LoggedInHeader";
+import React, { useState } from "react";
 
-export default function groceries() {
+export default function Groceries() {
+
+    const [groceries, setGroceries] = useState([]);
+    const [input, setInput] = useState();
+
+    const handleAddGroceries = () => {
+        const newGroceries = {
+            id: Math.random(), //Generate a unique ID for each item
+            text: input, //Get the text fdrom the input field
+            completed: false, //Initialize the item as not completed
+        };
+
+        setGroceries([...groceries, newGroceries]); //Add the new item to the list
+        setInput(''); //Clear the input field
+        }
+    
+    const handleToggleGroceries = (id) => {
+        setGroceries(groceries.map((groceries) => {
+            if (groceries.id === id) {
+                return { ...groceries, completed: !groceries.completed}; //Toggle the completed property
+            }
+            return groceries;
+        }));
+    };
+
     return (
         <>
         <div className="flex">
@@ -11,43 +36,28 @@ export default function groceries() {
         </div>
 
         <div className="flex flex-col justify-center items-center">
-            <label className="text-xl font-medium mt-3">Add item</label>
+            <label className="text-xl font-medium mt-3">Add your item</label>
             <input className="p-1 text-black mt-2 w-48" 
-                type="text" placeholder="Add item "></input>
+                type="text"
+                placeholder="Add item "
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                ></input>
             <button className="p-1 mt-4 bg-sky-400 w-48 hover:bg-sky-500 text-white rounded"
             onClick={() => {
-                console.log("Add button clicked")
+                handleAddGroceries()
             }}
             >Add</button>
         </div>
 
         <div className="flex flex-col justify-center items-center mt-10">
             <ul>
-                <li>
-                    <label>
-                        <input
-                            type="checkbox" className=""
-                            onChange={() => {
-                                console.log("Checkbox clicked")
-                            }}
-                            ></input>
-                            
-                    </label>
-                Bananas
-                </li>
-                <li>
-                    <label>
-                        <input
-                            type="checkbox"
-                            onChange={() => {
-                                console.log("Checkbox clicked")
-                            }}
-                            ></input>
-                            
-                    </label>
-                Milk
-                </li>
-                
+                {groceries.map((groceries) => (
+                    <li key={groceries.id}>
+                        <input type="checkbox" checked={groceries.completed} onChange={(e) => handleToggleGroceries(groceries.id)} />
+                        {groceries.text}
+                    </li>
+                ))}
             </ul>
         </div>
         </>
