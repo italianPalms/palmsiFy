3fc6f1df9863126b12cc6fb02cd3e38b1cad129a
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-11-23 22:50:31 +0100

    add styling to the list, possibility to use the enter button, capital letter, disable the add button if the input field is empty, add error handling when the input field is empty

--- src/pages/Groceries.jsx
+++ src/pages/Groceries.jsx
@@ -1,20 +1,33 @@
 import { LoggedInHeader } from "../components/LoggedInHeader";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Groceries() {
 
     const [groceries, setGroceries] = useState([]);
     const [input, setInput] = useState();
+    const [buttonDisabled, setButtonDisabled] = useState(true)
+    const [addGroceriesAttempted, setAddGroceriesAttempted] = useState(false);
 
     const handleAddGroceries = () => {
-        const newGroceries = {
-            id: Math.random(), //Generate a unique ID for each item
-            text: input, //Get the text fdrom the input field
-            completed: false, //Initialize the item as not completed
-        };
-
-        setGroceries([...groceries, newGroceries]); //Add the new item to the list
-        setInput(''); //Clear the input field
+        if(!input) {
+            setAddGroceriesAttempted(true);
+            return;
+        }
+        try {
+            const newGroceries = {
+                id: Math.random(), //Generate a unique ID for each item
+                text: input.charAt(0).toUpperCase() + input.slice(1), //Capitalize the input text
+                completed: false, //Initialize the item as not completed
+            };
+    
+            setGroceries([...groceries, newGroceries]); //Add the new item to the list
+            setInput(''); //Clear the input field
+            
+        } catch (error) {
+            console.log("Please add groceris" + error)
+        } finally {
+            setAddGroceriesAttempted(false);
+        }
         }
     
     const handleToggleGroceries = (id) => {
@@ -26,11 +39,26 @@ export default function Groceries() {
         }));
     };
 
+    const onAddGroceriesKeypress = e => {
+        if (e.keyCode === 13) {
+            handleAddGroceries();
+        }
+    }
+
+    useEffect(() => {
+        if (groceries.length > 0) {
+            setButtonDisabled(false); 
+        } else {
+            setButtonDisabled(true);
+        }
+    }, []);
+
     return (
         <>
         <div className="flex">
             <LoggedInHeader />
         </div>
+
         <div className="flex flex-col justify-center items-center mt-10">
             <h1 className="text-4xl font-bold">Groceries</h1>
         </div>
@@ -42,19 +70,26 @@ export default function Groceries() {
                 placeholder="Add item "
                 value={input}
                 onChange={(e) => setInput(e.target.value)}
+                onKeyDown={onAddGroceriesKeypress}
                 ></input>
             <button className="p-1 mt-4 bg-sky-400 w-48 hover:bg-sky-500 text-white rounded"
             onClick={() => {
                 handleAddGroceries()
             }}
             >Add</button>
+            {addGroceriesAttempted || groceries.lenght === 0 ? (
+                <p className="text-red-500">Please add groceries</p>
+                ) : null}
         </div>
 
         <div className="flex flex-col justify-center items-center mt-10">
-            <ul>
+            <ul className="text-xl">
                 {groceries.map((groceries) => (
-                    <li key={groceries.id}>
-                        <input type="checkbox" checked={groceries.completed} onChange={(e) => handleToggleGroceries(groceries.id)} />
+                    <li className="mt-1" key={groceries.id}>
+                        <input 
+                            className="mr-2 w-4 h-4"
+                            type="checkbox" 
+                            checked={groceries.completed} onChange={(e) => handleToggleGroceries(groceries.id)} />
                         {groceries.text}
                     </li>
                 ))}
