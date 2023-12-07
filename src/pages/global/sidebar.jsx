baa7f1b3f98e3564e02e7b92001a1c998e784d33
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-12-07 21:55:40 +0100

    add files for sidebar and topbar

---
+++ src/pages/global/sidebar.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { ProSidebar, Menu, MenuItem} from 'reaxt-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
+import { Box, IconButton, Typography, useTheme } from '@mui/material';
+import { Link } from 'react-router-dom';
+import TheatersOutlinedIcon from '@mui/icons-material/TheatersOutlined';
+import SmartToyOutlinedIcon from '@mui/icons-material/SmartToyOutlined';
+import GamesOutlinedIcon from '@mui/icons-material/GamesOutlined';
+import FastfoodOutlinedIcon from '@mui/icons-material/FastfoodOutlined';
+import GroupAddOutlinedIcon from '@mui/icons-material/GroupAddOutlined';
+
+
