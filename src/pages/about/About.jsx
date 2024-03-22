6395051c61aa7c17ed848093addc0e2355e0f880
Eirik Nordsveen <eiriknordsveen@gmail.com>
2024-03-22 19:53:07 +0100

    fix styling for about and investor page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,21 +1,24 @@
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import React from "react";
+import webDev from "../../img/webDev-img.webp";
 
 export default function About() {
 
     return (
         <>
-        <div className="min-h-screen flex flex-col">
+        <div>
             <Header />
-            <div className="flex-1">
-                <div className="flex flex-col font-medium items-center justify-center">
-                    <h1 className="text-3xl font-medium m-6 mt-28">About PalmsiFy</h1>
-                    <div className="p-2 ml-10 mr-10 max-w-2xl">
+                <div className="help-container">
+                    <h1>About PalmsiFy</h1>
+                    <div>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
+                    <div>
+                        {/* Fix img */}
+                        <img src="webDev"></img>
+                    </div>
                 </div>
-            </div>
             <Footer />
         </div>        
         </>
--- src/pages/investor/Investors.jsx
+++ src/pages/investor/Investors.jsx
@@ -4,20 +4,18 @@ import Footer from "../../components/Footer";
 export default function Investors () {
     return (
         <>
-        <div className="min-h-screen flex flex-col">
+        <div>
             <Header />
-            <div className="flex-1">
-                <div className="flex flex-col items-center justify-center">
-                    <h1 className="text-3xl font-medium m-6 mt-28">Welcome to our investor page</h1>
-                    <div className="mt-6 ml-10 mr-10 max-w-2xl">
+                <div className="help-container">
+                    <h1>Welcome to our investor page</h1>
+                    <div>
                         <p>Here you will find all relevant information that you as an Investors in PalmsiFy need.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. This page will be updated every quarter and an email will be sent out to all investors with all relevant information.</p>
-                        <p className="mt-6">Thank you so much for investing in PalmsiFy!</p>
+                        <p>Thank you so much for investing in PalmsiFy!</p>
                     </div>
-                <div>
-                    {/* add img */}
+                    <div>
+                        {/* add img */}
+                    </div>
                 </div>
-                </div>
-            </div>
             <Footer /> 
         </div>
         </>
