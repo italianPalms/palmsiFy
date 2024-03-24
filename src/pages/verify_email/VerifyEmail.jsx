0538540d84bf6810c88b18212e46b541fc9df100
Eirik Nordsveen <eiriknordsveen@gmail.com>
2024-03-24 18:57:25 +0100

    change port to make sure user get sent to right address when verifying their email, plus add styling for the verification page

--- src/pages/verify_email/VerifyEmail.jsx
+++ src/pages/verify_email/VerifyEmail.jsx
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
-
 export default function VerifyEmailPage() {
     const [token, setToken] = useState("");
     const [verified, setVerified] = useState(false);
@@ -35,21 +34,24 @@ export default function VerifyEmailPage() {
 
     return (
         <>
-        <div className='items-center justify-center flex flex-col min-h-screen p-2'>
-            <h1 className='text-4xl font-semibold'>Verify Email</h1>
-            {/* <h2 className='font-semibold text-2xl mt-8'>{token ? `${token}` : "no token"}</h2> */}
+        <div className='verifyEmail-container'>
+        <div>
+            <div className='verifyHeader'>
+            <h1>Verify Email</h1>
+            </div>
             {verified && (
-                <div>
-                    <h2 className='text-2xl bg-green-400 text-black p-2 mt-8 items-center justify-center flex w-66'>Email verified</h2>
-                    <a href='/login' className='font-medium text-base mt-8 items-center justify-center flex flex-col'>Click here to go to the login page</a>
+                <div className='success-error-container'>
+                    <h2 className='emailVerified'>Email verified</h2>
+                    <a href='/login' className='login_signup-link'>Click here to go to the login page</a>
                 </div>
             )}    
             {error && (
-                <div>
-                    <h2 className='text-3xl bg-red-400 text-black p-2 mt-8 w-66 items-center justify-center flex'>Error</h2>
-                    <p className='font-medium mt-4 flex items-center justify-center'>Something went wrong.Please try again!</p>
+                <div className='success-error-container'>
+                    <h2 className='emailVerificationError'>Error</h2>
+                    <p>Something went wrong. Please try again!</p>
                 </div>
             )}
+        </div>
         </div>
         </>
     )
